40a8ccf9f47bb29bebd45d8c1ced9199dec51680
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-04-25 15:16:59 +0300

    P02

---
+++ FirstStepsInCoding/FirstStepsInCodingLab/P2Nums1To10.js
@@ -0,0 +1,16 @@
+// Напишете функция, която отпечатва числата от 1 до 10 на отделни редове на конзолата.
+
+function num(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
+    console.log(6);
+    console.log(7);
+    console.log(8);
+    console.log(9);
+    console.log(10);
+}
+
+num();
