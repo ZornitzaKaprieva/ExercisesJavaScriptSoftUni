95a4e2b20f7495905586563e92a1a0affce64d28
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-04-25 15:18:00 +0300

    P03

---
+++ FirstStepsInCoding/FirstStepsInCodingLab/P03SquareArea.js
@@ -0,0 +1,12 @@
+// Да се напише функция, която получава цяло число 
+//и пресмята лицето на квадрат дадената страна. 
+
+function squareArea(input){
+
+    let a = Number(input[0]);
+
+    console.log(a * a);
+
+}
+
+squareArea("5");
