1547d32681c8e319b51aee76f266f3b6b9a346c3
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-04-25 15:15:34 +0300

    P01

---
+++ FirstStepsInCoding/FirstStepsInCodingLab/P01HelloSoftUni.js
@@ -0,0 +1,10 @@
+// Напишете функция, която отпечатва текста "Hello SoftUni".
+
+function hello(){
+    let hello = "Hello";
+    let uni = "SoftUni"
+
+    console.log(`Hello SoftUni`)
+
+}
+hello();
