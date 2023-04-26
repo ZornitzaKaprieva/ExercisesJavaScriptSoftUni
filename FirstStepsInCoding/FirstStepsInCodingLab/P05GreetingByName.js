1f25f2f629f2d6b1cd4419ab0b5d0f83bf2a1999
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-04-26 08:56:10 +0300

    P05

---
+++ FirstStepsInCoding/FirstStepsInCodingLab/P05GreetingByName.js
@@ -0,0 +1,11 @@
+// Напишете функция, която получава име на човек и отпечатва "Hello, <name>!", 
+//където <name> е въведеното име от конзолата.
+
+function greetingByName(input){
+
+    let name = input;
+
+    console.log(`Hello, ${name}!`);
+}
+
+greetingByName("Zornitza")
