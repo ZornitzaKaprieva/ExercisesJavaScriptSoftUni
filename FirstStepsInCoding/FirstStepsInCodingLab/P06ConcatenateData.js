3a824e7d8c2ac0d9dd8d1e2ac6af54368099a5c0
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-04-26 08:56:55 +0300

    P06

---
+++ FirstStepsInCoding/FirstStepsInCodingLab/P06ConcatenateData.js
@@ -0,0 +1,14 @@
+// Напишете функция, която получава: име, фамилия, възраст и град и печата съобщение от следния вид: 
+// "You are <firstName> <lastName>, a <age>-years old person from <town>."
+
+function concatenateData(input){
+    
+    let firstName = input[0];
+    let lastName = input[1];
+    let age = Number(input[2]);
+    let town = input[3];
+
+    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
+}
+
+concatenateData(["Zornitza", "Kaprieva", "37", "Sofia"],)
