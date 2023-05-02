0cdde58029961754da0ef9731dd54b02bdd68ce5
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-05-02 08:14:46 +0300

    P04

---
+++ ConditionalStatements/ConditionalStatementsLab/P04PasswordGuess.js
@@ -0,0 +1,18 @@
+// Да се напише функция, която получава парола (текст) 
+// и проверява дали дадената паролата съвпада с фразата "s3cr3t!P@ssw0rd". 
+// При съвпадение да се изведе "Welcome". При несъвпадение да се изведе "Wrong password!". 
+
+function passwordGuess(input){
+
+    let pass = input[0];
+
+    let correctpPass = "s3cr3t!P@ssw0rd"
+
+    if(pass === correctpPass){
+        console.log("Welcome")
+    }  else {
+        console.log("Wrong password!")
+    }
+}
+
+passwordGuess(["s3cr3t!P@ssw0rd"])
