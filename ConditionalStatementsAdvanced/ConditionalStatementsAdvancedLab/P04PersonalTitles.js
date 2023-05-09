fbc63b697a6389f49fd273121de56b1fa1b13ae2
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-05-09 08:29:16 +0300

    P04

---
+++ ConditionalStatementsAdvanced/ConditionalStatementsAdvancedLab/P04PersonalTitles.js
@@ -0,0 +1,30 @@
+// Да се напише функция, която получава възраст (реално число) и пол ('m' или 'f'), въведени от потребителя, и отпечатва обръщение измежду следните:
+// •	"Mr." – мъж (пол 'm') на 16 или повече години
+// •	"Master" – момче (пол 'm') под 16 години
+// •	"Ms." – жена (пол 'f') на 16 или повече години
+// •	"Miss" – момиче (пол 'f') под 16 години
+
+function personalTitles(input) {
+
+    let age = Number(input[0]);
+    let sex = input[1];
+
+    switch (sex) {
+        case "f":
+            if (age < 16) {
+                console.log("Miss");
+            } else {
+                console.log("Ms.");
+            }
+            break;
+        case "m":
+            if (age < 16) {
+                console.log("Master");
+            } else {
+                console.log("Mr.");
+            }
+            break;
+    }
+}
+
+personalTitles(["12", "f"]);
