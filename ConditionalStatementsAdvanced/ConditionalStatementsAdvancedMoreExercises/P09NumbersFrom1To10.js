f8328b32663b9f9c95103bae32d8d12d2e802dc0
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-05-17 08:37:18 +0300

    P09

---
+++ ConditionalStatementsAdvanced/ConditionalStatementsAdvancedMoreExercises/P09NumbersFrom1To10.js
@@ -0,0 +1,10 @@
+// Напишете програма, която отпечатва числата от 1 до 10, по едно на ред.
+
+function numbersFrom1To10(){
+
+    for(let i = 1; i <= 10; i++){
+        console.log(i);
+    }
+}
+
+numbersFrom1To10();
