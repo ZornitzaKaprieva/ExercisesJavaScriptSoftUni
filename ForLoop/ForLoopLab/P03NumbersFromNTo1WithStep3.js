56354cef3d5187c9687aa453e1c186b513fe641c
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-05-18 08:45:28 +0300

    P03

---
+++ ForLoop/ForLoopLab/P03NumbersFromNTo1WithStep3.js
@@ -0,0 +1,11 @@
+// Напишете функция, която получава число n и отпечатва числата от 1 до n през 3.
+
+function numbersFromNTo1WithStep3(input) {
+  let n = Number(input[0]);
+
+  for (let i = 1; i <= n; i += 3) {
+    console.log(i);
+  }
+}
+
+numbersFromNTo1WithStep3(["10"]);
