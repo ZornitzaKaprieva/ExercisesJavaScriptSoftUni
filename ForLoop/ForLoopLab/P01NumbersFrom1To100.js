71c17bd2cc9986f2b8d206625198cd26cdcb4236
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-05-18 08:44:30 +0300

    P01

---
+++ ForLoop/ForLoopLab/P01NumbersFrom1To100.js
@@ -0,0 +1,9 @@
+// Напишете функция, която отпечатва числата от 1 до 100, всяко на нов ред.
+
+function numbersFrom1To100() {
+  for (let i = 1; i <= 100; i++) {
+    console.log(i);
+  }
+}
+
+numbersFrom1To100();
