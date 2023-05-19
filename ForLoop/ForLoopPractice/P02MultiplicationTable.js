ac7769bf5361b4e2e1da3746066a13aeaa3ab2d4
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-05-19 08:30:02 +0300

    P02

---
+++ ForLoop/ForLoopPractice/P02MultiplicationTable.js
@@ -0,0 +1,12 @@
+// Напишете функция, която получава аргумент число от 1 до 10 и принтира таблицата за умножение в конзолата.
+
+function multiplicationTable(input) {
+  let num = Number(input[0]);
+
+  for (let i = 1; i <= 10; i++) {
+    let result = i * num;
+    console.log(`${i} * ${num} = ${result}`);
+  }
+}
+
+multiplicationTable(["5"]);
