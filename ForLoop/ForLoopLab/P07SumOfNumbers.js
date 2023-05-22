7918e2b3b988458f45cc80d8878148af5abcc89a
Zornitza Kaprieva <119599133+ZornitzaKaprieva@users.noreply.github.com>
2023-05-22 09:17:03 +0300

    P07

--- ForLoop/ForLoopLab/P07SumOfNumbers.js
+++ ForLoop/ForLoopLab/P07SumOfNumbers.js
@@ -5,14 +5,17 @@ function sumOfNumbers(string) {
   let n = string[0];
   let sum = 0;
 
-  for (let i = 0; i <= n.length; i++) {
+  for (let i = 0; i < n.length; i++) {
     sum = sum + Number(n.charAt(i));
+
+    // let currentNum = Number(n[i]);
+    // sum += currentNum;
   }
 
   console.log(`The sum of the digits is:${sum}`);
 }
 
-sumOfNumbers(["564891"]);
+sumOfNumbers(["12345"]);
 
 //Solution 2:
 function sumOfNumbersN(number) {
