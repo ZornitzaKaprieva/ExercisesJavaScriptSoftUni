1467026e7056d94ec4cb422c6e4faf02b0c59845
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-05-23 08:46:22 +0300

    P02-2

---
+++ ForLoop/ForLoopPractice/P05SalarySolution2.js
@@ -0,0 +1,65 @@
+// Шеф на компания забелязва че все повече служители прекарват  време в сайтове, които ги разсейват.
+// За да предотврати това, той въвежда изненадващи проверки на отворените табове на браузъра на служителите си.
+// Според отворения сайт в таба се налагат следните глоби:
+// •	"Facebook" -> 150 лв.
+// •	"Instagram" -> 100 лв.
+// •	"Reddit" -> 50 лв.
+// От конзолата се четат два реда:
+// •	Брой отворени табове в браузъра n - цяло число в интервала [1...10]
+// •	Заплата - число в интервала [500...1500]
+// След това n – на брой пъти се чете име на уебсайт – текст
+// Изход
+// •	Ако по време на проверката заплатата стане по-малка или равна на 0 лева, на конзолата се изписва
+// "You have lost your salary." и програмата приключва.
+// •	В противен случай след проверката на конзолата се изписва остатъкът от заплатата (да се изпише като цяло число).
+
+function salary(input) {
+  let openTabs = Number(input[0]);
+  let salary = Number(input[1]);
+
+  let facebookCount = 0;
+  let instagramCount = 0;
+  let redditCount = 0;
+
+  for (let i = 0; i <= input.length; i++) { //i <= openTabs
+    let website = input[i];
+
+    switch (website) {
+      case "Facebook":
+        facebookCount++;
+        break;
+      case "Instagram":
+        instagramCount++;
+        break;
+      case "Reddit":
+        redditCount++;
+        break;
+    }
+  }
+
+  let facebookFine = facebookCount * 150;
+  let instagramFine = instagramCount * 100;
+  let redditFine = redditCount * 50;
+
+  let fine = facebookFine + instagramFine + redditFine;
+
+  let diff = salary - fine;
+
+  if (salary > fine) {
+    console.log(diff.toFixed(0));
+  } else {
+    console.log("You have lost your salary.");
+  }
+}
+
+salary([
+  "10",
+  "750",
+  "Facebook",
+  "Dev.bg",
+  "Instagram",
+  "Facebook",
+  "Reddit",
+  "Facebook",
+  "Facebook",
+]);
