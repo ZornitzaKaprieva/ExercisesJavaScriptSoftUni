47886e6fae349956f0c13d499fcd61b5cde155b7
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-08-14 09:23:39 +0300

    P05

--- ForLoop/ForLoopLab/P05CharacterSequence.js
+++ ForLoop/ForLoopLab/P05CharacterSequence.js
@@ -3,14 +3,10 @@
 function characterSequence(input) {
   let text = input[0];
 
-  // console.log(text);
-  // console.log(text[0]);
-
   let txtLength = text.length;
 
   for (i = 0; i < txtLength; i++) {
     console.log(text[i]);
-    //console.log(text.charAt(i));
   }
 }
 
