566125a5d4e8aaa34e752563ddd06e29a3a7af3f
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-08-14 09:26:02 +0300

    P06

--- ForLoop/ForLoopLab/P06VowelsSum.js
+++ ForLoop/ForLoopLab/P06VowelsSum.js
@@ -5,11 +5,9 @@
 
 function vowelsSum(input) {
   let text = input[0];
+
   sum = 0;
   for (let i = 0; i < text.length; i++) {
-    // console.log(text[i]);
-    //console.log(text.charAt(i));
-
     switch (text.charAt(i)) {
       case "a":
         sum = sum + 1;
