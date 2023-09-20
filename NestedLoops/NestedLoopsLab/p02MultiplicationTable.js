148bae306ed89537edaeba31434ff1789a23a553
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-09-20 14:56:36 +0300

    P02

---
+++ NestedLoops/NestedLoopsLab/p02MultiplicationTable.js
@@ -0,0 +1,14 @@
+// Отпечатайте на конзолата таблицата за умножение за числата от 1 до 10 във формат:
+// "{първи множител} * {втори множител} = {резултат}".
+
+function multiplicationTable() {
+    
+  for (let x = 1; x <= 10; x++) {
+    for (let y = 1; y <= 10; y++) {
+      let result = x * y;
+      console.log(`${x} * ${y} = ${result}`);
+    }
+  }
+}
+
+multiplicationTable();
