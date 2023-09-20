8ea662b64c10266d032e1a940768c5fa95917379
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-09-20 14:57:06 +0300

    P03

---
+++ NestedLoops/NestedLoopsLab/p03Combinations.js
@@ -0,0 +1,24 @@
+// Напишете функция, която изчислява колко решения в естествените числа (включително и нулата) има уравнението:
+// x1 + x2 + x3 = n
+// Числото n е цяло число и се въвежда от конзолата.
+
+function combinations(input) {
+  let n = Number(input[0]);
+
+  let counter = 0;
+
+  for (let x1 = 0; x1 <= n; x1++) {
+    for (let x2 = 0; x2 <= n; x2++) {
+      for (let x3 = 0; x3 <= n; x3++) {
+        let result = x1 + x2 + x3;
+
+        if (result === n) {
+          counter++;
+        }
+      }
+    }
+  }
+  console.log(counter);
+}
+
+combinations(["25"]);
